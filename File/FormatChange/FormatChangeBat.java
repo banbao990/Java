6fa52cf16bb9845ee6e2b469cefa06f24dc7d571
“banbao990 <2369896555@qq.com>
2020-09-17 14:30:39 +0800

    version 2(add regex filter)
    version 2(add regex filter)

--- File/FormatChange/FormatChangeBat.java
+++ File/FormatChange/FormatChangeBat.java
@@ -4,15 +4,19 @@ import java.io.File;
  * @author banbao
  * @version 1
  * 1.1 批量文件格式转换
+ * @version 2
+ * 2.1 加入正则表达式过滤
  */
 
 public class FormatChangeBat{
     static FormatChange fc = new FormatChange();
     static String format1, format2;
     static int total = 0;
+    static String regex = "";
     // 接受命令行参数
     public static void main(String...args) throws Exception{
-        if(args.length == 2) {
+        if(args.length == 3) {
+            regex = args[2];
             if("U2G".equals(args[0])) {
                 format1 = "UTF-8"; format2 = "GBK";
             }
@@ -23,7 +27,8 @@ public class FormatChangeBat{
             }
             change(new File(args[1])); // Exception
         } else {
-            System.err.println("usage:java FormatChangeBat <U2G|G2U> dirName\n");
+            System.err.println("usage:java FormatChangeBat <U2G|G2U> dirName regex\n");
+            System.exit(1);
         }
         System.out.println("total:" + total);
     }
@@ -37,6 +42,7 @@ public class FormatChangeBat{
         for(String fileInDir : dir.list()){
             File file = new File(dir, fileInDir);
             if(file.isFile()){
+                if(!fileInDir.matches(regex)) { continue; }
                 fc.change(dir + "\\" + file.getName(), null, format1, format2);
                 ++total;
             }else{
